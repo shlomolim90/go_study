197c2259f1c03581cb2fe8cd409920e60d6beb54
임재환 <shlomolim90@gmail.com>
2024-11-28 22:34:15 +0900

    Study Type Conversion

--- simple_module/type_example.go
+++ simple_module/type_example.go
@@ -1,12 +1,12 @@
-
 package simple_module
 
 import (
     "fmt"
     "unsafe"
+    "strconv"
 )
 
-
+// 본 파일에서는 Type Casting, Type Assertion 에 대해서 배운다.
 
 func TestUintptr() {
     var x   int     = 10
@@ -32,7 +32,65 @@ func TestUintptr() {
     fmt.Printf("uintptr u: %x\n", u)
 }
 
+type test interface {
+    GetName()   string
+}
+
+type Test struct {
+    Name    string
+    Age     int
+}
+
+func (t *Test) GetName() string {
+    return t.Name
+}
+
+func TestTypeCasting() {
+    var i   test
+    var i2  interface{}
+    var t   Test
+    t.Name = "LIM"
+    t.Age = 3
+
+    i2 = &t
+    i = &t
+
+    fmt.Printf("%s\n", i.GetName())
+    fmt.Printf("%s\n", i2.(test).GetName())
+}
+
+func TestTypeCasting2() {
+    var i   Test
+    i.Name = "LEE"
+    i.Age = 10
+
+    var i2  interface{}
+
+    // Test 구조체를 가리키는 포인터변수를 interface{} 타입으로 캐스팅 = <type>()
+    // 참고: &i 을 interface 로 캐스팅하는 이유는 아래 GetName 함수의 receiver 가 pointer 형식이기 때문.
+    // 참고: Go 용어로 Casting 이라고 함.
+    i2 = interface{}(&i)
+
+    // interface{} 타입을 test interface 로 캐스팅 = <from_interface>.(<to_interface>)
+    // 참고: Go 용어로 "Type Assertion" 이라고 함.
+    fmt.Printf("%s\n", i2.(test).GetName())
+}
+
+func TestStrConversion() {
+    // strconv 는 문자열 전환에 특화된 패키지.
+    i, err := strconv.Atoi("100")
+    fmt.Printf("i:%d, err:%v\n", i, err)
+
+    i, err = strconv.Atoi("s100")
+    fmt.Printf("i:%d, err:%v\n", i, err)
+
+    str := strconv.Itoa(231)
+    fmt.Printf("str:%s\n", str)
+}
 
 func TestType() {
     TestUintptr()
+    TestTypeCasting()
+    TestTypeCasting2()
+    TestStrConversion()
 }
